c6d028e172640eccde8f228331c4ca535a2ba8bf
Puneethraj K <79778679+Puneethraj0811@users.noreply.github.com>
2023-01-03 16:44:44 +0530

    Add files via upload

---
+++ 3rd-Sem/Advance_Java/8_Student_Info/build/generated/src/org/apache/jsp/file2_jsp.java
@@ -0,0 +1,98 @@
+package org.apache.jsp;
+
+import javax.servlet.*;
+import javax.servlet.http.*;
+import javax.servlet.jsp.*;
+
+public final class file2_jsp extends org.apache.jasper.runtime.HttpJspBase
+    implements org.apache.jasper.runtime.JspSourceDependent {
+
+  private static final JspFactory _jspxFactory = JspFactory.getDefaultFactory();
+
+  private static java.util.List<String> _jspx_dependants;
+
+  private org.glassfish.jsp.api.ResourceInjector _jspx_resourceInjector;
+
+  public java.util.List<String> getDependants() {
+    return _jspx_dependants;
+  }
+
+  public void _jspService(HttpServletRequest request, HttpServletResponse response)
+        throws java.io.IOException, ServletException {
+
+    PageContext pageContext = null;
+    HttpSession session = null;
+    ServletContext application = null;
+    ServletConfig config = null;
+    JspWriter out = null;
+    Object page = this;
+    JspWriter _jspx_out = null;
+    PageContext _jspx_page_context = null;
+
+    try {
+      response.setContentType("text/html;charset=UTF-8");
+      pageContext = _jspxFactory.getPageContext(this, request, response,
+      			null, true, 8192, true);
+      _jspx_page_context = pageContext;
+      application = pageContext.getServletContext();
+      config = pageContext.getServletConfig();
+      session = pageContext.getSession();
+      out = pageContext.getOut();
+      _jspx_out = out;
+      _jspx_resourceInjector = (org.glassfish.jsp.api.ResourceInjector) application.getAttribute("com.sun.appserv.jsp.resource.injector");
+
+      out.write("\n");
+      out.write("\n");
+      out.write("\n");
+      out.write("<!DOCTYPE html>\n");
+      out.write("<html>\n");
+      out.write("    <head>\n");
+      out.write("        <meta http-equiv=\"Content-Type\" content=\"text/html; charset=UTF-8\">\n");
+      out.write("        <title>JSP Page</title>\n");
+      out.write("    </head>\n");
+      out.write("    <body style=\"background-color: grey\">\n");
+      out.write("    <center><br><br><br><br>\n");
+      out.write("        <h2><b>STUDENT DETAILS RETRIEVED FROM STUDENT BEAN</b></h2> \n");
+      out.write("        ");
+      program.StudentBean stud = null;
+      synchronized (session) {
+        stud = (program.StudentBean) _jspx_page_context.getAttribute("stud", PageContext.SESSION_SCOPE);
+        if (stud == null){
+          stud = new program.StudentBean();
+          _jspx_page_context.setAttribute("stud", stud, PageContext.SESSION_SCOPE);
+        }
+      }
+      out.write("\n");
+      out.write("        <table>\n");
+      out.write("            <tr><td>STUDENT NAME:</td><td>");
+      out.write(org.apache.jasper.runtime.JspRuntimeLibrary.toString((((program.StudentBean)_jspx_page_context.findAttribute("stud")).getName())));
+      out.write("</td></tr>\n");
+      out.write("            <tr><td>USN:</td><td>");
+      out.write(org.apache.jasper.runtime.JspRuntimeLibrary.toString((((program.StudentBean)_jspx_page_context.findAttribute("stud")).getUsn())));
+      out.write("</td></tr>\n");
+      out.write("            <tr><td>BRANCH:</td><td>");
+      out.write(org.apache.jasper.runtime.JspRuntimeLibrary.toString((((program.StudentBean)_jspx_page_context.findAttribute("stud")).getBranch())));
+      out.write("</td></tr>\n");
+      out.write("            <tr><td>EMAIL ID:</td><td>");
+      out.write(org.apache.jasper.runtime.JspRuntimeLibrary.toString((((program.StudentBean)_jspx_page_context.findAttribute("stud")).getEmail())));
+      out.write("</td></tr>\n");
+      out.write("            <tr><td>CONTACT NUMBER:</td><td>");
+      out.write(org.apache.jasper.runtime.JspRuntimeLibrary.toString((((program.StudentBean)_jspx_page_context.findAttribute("stud")).getPhone())));
+      out.write("</td></tr>\n");
+      out.write("        </table>\n");
+      out.write("    </center>\n");
+      out.write("    </body>\n");
+      out.write("</html>\n");
+    } catch (Throwable t) {
+      if (!(t instanceof SkipPageException)){
+        out = _jspx_out;
+        if (out != null && out.getBufferSize() != 0)
+          out.clearBuffer();
+        if (_jspx_page_context != null) _jspx_page_context.handlePageException(t);
+        else throw new ServletException(t);
+      }
+    } finally {
+      _jspxFactory.releasePageContext(_jspx_page_context);
+    }
+  }
+}
