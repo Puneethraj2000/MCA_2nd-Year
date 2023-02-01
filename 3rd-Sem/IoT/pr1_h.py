ba4af140e9bc672251354b2e2deced8416ae23d1
Puneethraj K <79778679+Puneethraj0811@users.noreply.github.com>
2023-02-01 20:07:29 +0530

    Delete pr1_h.py

--- 3rd-Sem/IoT/pr1_h.py
+++
@@ -1,8 +0,0 @@
-#Display the number of lines and number of words in a each line
-
-fl = open(input("Enter the file name:"))
-count = 0
-for i in fl:
-	lst = i.split()
-	count +=1
-	print("The number of words in the line",count,"is",len(lst))
